9d9a65180a3b0bbf18acfd7b4dbc056d412ccf80
Toothless2 <max.rose366@gmail.com>
2020-07-26 22:26:19 +0100

    es

--- db/migrate/20200726193857_actually_migrate_record_cols.rb
+++ db/migrate/20200726193857_actually_migrate_record_cols.rb
@@ -1,8 +1,8 @@
 class ActuallyMigrateRecordCols < ActiveRecord::Migration[6.0]
   def change
     remove_column :records, :bowstyle
-    remove_column :records, :round
-    change_column :records, :bowstyle, :integer
-    change_column :records, :round, :integer
+    remove_column :records, :round # have to add then remove to make heroku happy
+    add_column :records, :bowstyle, :integer
+    add_column :records, :round, :integer
   end
 end
