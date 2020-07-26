70e130fff5db092e23c5a546fcd4abbf0babac2f
Toothless2 <max.rose366@gmail.com>
2020-07-26 22:20:19 +0100

    es

--- db/migrate/20200726193857_actually_migrate_record_cols.rb
+++ db/migrate/20200726193857_actually_migrate_record_cols.rb
@@ -1,5 +1,7 @@
 class ActuallyMigrateRecordCols < ActiveRecord::Migration[6.0]
   def change
+    delete_column :records, :bowstyle
+    delete_column :records, :round
     change_column :records, :bowstyle, :integer
     change_column :records, :round, :integer
   end
