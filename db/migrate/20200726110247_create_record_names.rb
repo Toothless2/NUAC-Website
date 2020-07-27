93be836b105c35a0aa5c816ce35afd45d8bcc538
Toothless2 <max.rose366@gmail.com>
2020-07-27 23:37:43 +0100

    changed some things =

--- db/migrate/20200726110247_create_record_names.rb
+++ db/migrate/20200726110247_create_record_names.rb
@@ -1,7 +1,7 @@
 class CreateRecordNames < ActiveRecord::Migration[6.0]
   def change
     create_table :record_names do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: true, foreign_key: true
       t.string :name
 
       t.timestamps
