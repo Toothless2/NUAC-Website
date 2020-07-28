3d15d17835674a661c9f57f2bd08ca0e49416256
Toothless2 <max.rose366@gmail.com>
2020-07-28 21:34:47 +0100

    nothing important

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -18,16 +18,5 @@ class ContactUsController < ApplicationController
         format.html { render :contactus, notice: 'Failed to send' }
       end
     end
-
-    # if /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(contactus[:email]) == nil # Check its a valid email
-    #   flash.now[:error] = 'Cannot send message invalid email'
-    #   render :contactus
-    # elsif verify_recaptcha(contactus) && ContactMailer.contact_us(contactus).deliver_now
-    #   flash.now[:error] = nil
-    #   redirect_to contactus_path, notice: 'Message sent successfully'
-    # else
-    #   flash.now[:error] = 'Cannot send message'
-    #   render :contactus
-    # end
   end
 end
