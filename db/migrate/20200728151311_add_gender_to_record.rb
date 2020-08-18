1e044d91148c5d0306fd65ef5bd11467d19bfb3f
Toothless2 <max.rose366@gmail.com>
2020-08-18 13:04:19 +0100

    heroku fixes?

--- db/migrate/20200728151311_add_gender_to_record.rb
+++ db/migrate/20200728151311_add_gender_to_record.rb
@@ -1,5 +1,5 @@
 class AddGenderToRecord < ActiveRecord::Migration[6.0]
   def change
-    add_column :records, :gender, :boolean
+    add_column :records, :gender, :integer
   end
 end
--- db/migrate/20200815191834_record_gender_to_int.rb
+++
@@ -1,5 +0,0 @@
-class RecordGenderToInt < ActiveRecord::Migration[6.0]
-  def change
-    change_column :records, :gender, :integer
-  end
-end
