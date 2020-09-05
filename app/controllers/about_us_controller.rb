fa934f8d4b499df60ac306094a5c60997dcc9a8c
Toothless2 <max.rose366@gmail.com>
2020-09-05 19:33:49 +0100

    updated as per meeting

--- app/controllers/about_us_controller.rb
+++ app/controllers/about_us_controller.rb
@@ -2,4 +2,9 @@ class AboutUsController < ApplicationController
   def aboutus
     @committees = Committee.all
   end
+
+  private
+    def committee_user?
+      super || current_user&.committee?
+    end
 end
